88896702779f51b2b25186975fbcae139f7812a0
Matias Ezequiel Fontenla <78578011+matiascae@users.noreply.github.com>
2022-07-07 16:04:35 -0300

    Update editarAcerca.js

--- js/editarAcerca.js
+++ js/editarAcerca.js
@@ -1,15 +1,15 @@
 function cambiar_parrafo(){
-    document.getElementById("edit-acercade").style.display="block"
-    document.getElementById("eliminar_acerca").style.display="block"
+    document.getElementById("edit-acercade").style.display="block";
+    document.getElementById("eliminar_acerca").style.display="block";
     document.getElementById("editar_acerca").style.display ="none";
     let texto = document.getElementById("text-acercade").innerText;
     console.log(texto)
 }
 
 function parrafo_editado(){
-    document.getElementById("edit-acercade").style.display="none"
+    document.getElementById("edit-acercade").style.display="none";
 }
 function myFunction2(valor){
-        document.getElementById('text-acercade').innerHTML = valor
-        document.getElementById("editar_acerca").style.display="none"
+        document.getElementById('text-acercade').innerHTML = valor;
+        document.getElementById("editar_acerca").style.display="none";
         }
