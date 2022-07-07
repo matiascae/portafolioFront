7e0cf634054ece3eb3544af2420ae3321a9e27cf
Matias Fontenla <matiasfontenla1994@gmail.com>
2022-07-07 20:40:15 -0300

    prueba1

--- js/editarAcerca.js
+++ js/editarAcerca.js
@@ -1,19 +1,18 @@
 function cambiar_parrafo(){
-<<<<<<< HEAD
+
     document.getElementById("edit-acercade").style.display="block"
     document.getElementById("eliminar_acerca").style.display="block"
     document.getElementById("editar-png").style.display ="none";
-=======
     document.getElementById("edit-acercade").style.display="block";
     document.getElementById("eliminar_acerca").style.display="block";
     document.getElementById("editar_acerca").style.display ="none";
->>>>>>> 88896702779f51b2b25186975fbcae139f7812a0
+
     let texto = document.getElementById("text-acercade").innerText;
     console.log(texto)
 }
 
 function parrafo_editado(){
-<<<<<<< HEAD
+
     document.getElementById("edit-acercade").style.display="none"
     document.getElementById("eliminar_acerca").style.display="none"
 }
@@ -21,11 +20,11 @@ function myFunction2(valor){
         document.getElementById('text-acercade').innerHTML = valor
         document.getElementById("editar_acerca").style.display="none";
         }
-=======
+
     document.getElementById("edit-acercade").style.display="none";
-}
+
 function myFunction2(valor){
         document.getElementById('text-acercade').innerHTML = valor;
         document.getElementById("editar_acerca").style.display="none";
         }
->>>>>>> 88896702779f51b2b25186975fbcae139f7812a0
+
